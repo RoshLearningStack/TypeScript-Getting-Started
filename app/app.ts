693afaed7a17dfbfa87235e4bfd4268a4678db8f
Roshan Deniyage <crdeniyage@gmail.com>
2020-09-05 03:20:54 +1200

    Custom types

--- app/app.ts
+++ app/app.ts
@@ -1,46 +1,76 @@
-function startGame() {
-    // starting a new game
+/// <reference path="player.ts" />
+/// <reference path="game.ts" />
 
-    let playerName: string | undefined = getInputValue('playername');
-    logPlayer(playerName);
-    postScore(80, playerName);
-    postScore(-5, playerName);
+// function startGame() {
+//     // starting a new game
+
+//     let playerName: string | undefined = getInputValue('playername');
+//     logPlayer(playerName);
+//     postScore(80, playerName);
+//     postScore(-5, playerName);
     
-}
+// }
 
-function logPlayer(name:string = 'MultiMath Player') :void {
-    console.log(`Name game starting for player: ${name }`);
-}
+// function logPlayer(name:string = 'MultiMath Player') :void {
+//     console.log(`Name game starting for player: ${name }`);
+// }
 
-function getInputValue(elementId:string): string | undefined {
-     const inputElement : HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
-     if(inputElement.value === '') {
-         return undefined;
-     } else {
-         return inputElement.value;
-     }
-}
+// function postScore(score:number, playerName: string = 'MultiMath Player') : void {
+//     // Variable to reference functions with string parameter and no return type
+//     let logger : (value: string) => void;
+//     if(score < 0 ) {
+//         logger = logError;
+//     } else  {
+//         logger = logMessage;
+//     }
 
-function postScore(score:number, playerName: string = 'MultiMath Player') : void {
-    // Variable to reference functions with string parameter and no return type
-    let logger : (value: string) => void;
-    if(score < 0 ) {
-        logger = logError;
-    } else  {
-        logger = logMessage;
-    }
+//     const scoreElement : HTMLElement | null = document.getElementById('postedScores');
+//     scoreElement!.innerText = `${score} - ${playerName}`;
+//     logger(`Score: ${score }`);
+// }
 
-    const scoreElement : HTMLElement | null = document.getElementById('postedScores');
-    scoreElement!.innerText = `${score} - ${playerName}`;
-    logger(`Score: ${score }`);
-}
+// document.getElementById('startGame')!.addEventListener('click',  startGame);
 
-document.getElementById('startGame')!.addEventListener('click',  startGame);
+// // Arrow Functions
+// const  logMessage = (message:string) => console.log(message);
+// // logMessage('Welcome to MultiMath');
 
-// Arrow Functions
-const  logMessage = (message:string) => console.log(message);
-// logMessage('Welcome to MultiMath');
+// function logError(error:string): void{
+//     console.error(error);
+// }
 
-function logError(error:string): void{
-    console.error(error);
-}
+// // let myResult : Result = {
+// //     playerName: 'Marie',
+// //     score: 5,
+// //     problemCount: 5,
+// //     factor: 7
+// // }
+
+// // let player : Person = {
+// //     name: 'Roshan ',
+// //     formatName: () => 'Dan'
+// // }
+
+// const firstPlayer : Player = new Player();
+// firstPlayer.name = 'Roshan ';
+// console.log(firstPlayer.formatName());
+
+let newGame: Game;
+
+// add click handler to the start game button
+document.getElementById('startGame')!.addEventListener('click', () => {
+    const player: Player = new Player();
+    player.name = Utility.getInputValue('playername');
+
+    const problemCount: number = Number(Utility.getInputValue('problemCount'));
+    const factor: number = Number(Utility.getInputValue('factor'));
+
+    newGame = new Game(player, problemCount, factor);
+    newGame.displayGame();
+});
+
+// add click handler to the calculate score button
+
+document.getElementById('calculate')!.addEventListener('click', () => {
+    newGame.calculateScore();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  entry: './app/app.ts',
-  devtool: 'inline-source-map',
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'bundle.js'
-  },
+  entry: './js/app.js',
+  // devtool: 'inline-source-map',
+  // resolve: {
+  //   extensions: [ '.tsx', '.ts', '.js' ]
+  // },
+  // output: {
+  //   filename: 'bundle.js'
+  // },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.tsx?$/,
+  //       use: 'ts-loader',
+  //       exclude: /node_modules/
+  //     }
+  //   ]
+  // },
   devServer: {
     inline: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
   }
 };
