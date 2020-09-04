446fea16862b3c94d2be9c7ea1db33f614f4bab1
Roshan Deniyage <crdeniyage@gmail.com>
2020-09-04 18:49:36 +1200

    Set up dev env

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,11 @@ module.exports = {
   },
   module: {
     rules: [
-      {test: /\.ts$/, use: 'ts-loader'}
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
     ]
   }
 };
