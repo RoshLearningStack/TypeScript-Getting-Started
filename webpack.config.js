bdf932e9aba50cdccda9eee09912fec146c9cf47
Roshan Deniyage <crdeniyage@gmail.com>
2020-09-05 16:44:56 +1200

    Module bundling with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  entry: './js/app.js',
-  // devtool: 'inline-source-map',
-  // resolve: {
-  //   extensions: [ '.tsx', '.ts', '.js' ]
-  // },
-  // output: {
-  //   filename: 'bundle.js'
-  // },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.tsx?$/,
-  //       use: 'ts-loader',
-  //       exclude: /node_modules/
-  //     }
-  //   ]
-  // },
+  entry: './app/app.ts',
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
   devServer: {
     inline: false
   }
